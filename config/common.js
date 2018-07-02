2339710efe08659628ad334b298e6c8314868615
Roam Tang <roam.tang@sap.com>
2018-07-02 15:16:21 +0800

    server change

--- config/common.js
+++ config/common.js
@@ -8,7 +8,7 @@ module.exports = {
     encodingAESKey: 'qKZrS7ClCedpfB8ovtIXaOUlnUyd4JhDH44v3STxPyq'
   },
   server: 'http://118.24.149.237',
-  uiServer: home ? 'http://10.130.231.80' : 'http://118.24.149.237',
+  uiServer: home ? 'http://10.130.231.80' : 'http://118.24.149.237:8080',
   templates: {
     Featured: '04zVisr--LdimrcGC6qNbExIkUyrn0e3tAYQRbrtvF8',
     NewBlog: 'ZKAOh-mi-r-SV7cHjxJQEyt1UUWgXD8VRyAMvV_KJCQ',
