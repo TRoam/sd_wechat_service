905e301640b86edf8afb75173182ad93d2d632b5
Roam Tang <liulangdewoniu@gmail.com>
2018-07-03 16:55:45 +0800

    crose domain

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var order = require('./routes/order');
 var app = express();
 
 app.use(function(req, res, next) {
-//  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://ui.chengdusa.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
